b1f0c621440664bcc2c66768f6ed49f4083493f6
Adriaan Thomas <info@adriaanthomas.nl>
2021-12-06 20:17:14 +0100

    Day 2 part 2

--- Day2.fs
+++ Day2.fs
@@ -8,8 +8,18 @@ type Move =
     | Down
     | Up
 
-type Instruction = { Move: Move; Distance: int }
-type Position = { Horizontal: int; Depth: int }
+type Instruction =
+    { Move: Move
+      Distance: int }
+    override x.ToString() =
+        $"{{ Move={x.Move}; Distance={x.Distance} }}"
+
+type Position =
+    { Horizontal: int
+      Depth: int
+      Aim: int }
+    override x.ToString() =
+        $"{{ Horizontal={x.Horizontal}; Depth={x.Depth}; Aim={x.Aim} }}"
 
 let parseMove move =
     match move with
@@ -27,26 +37,45 @@ let parseLine (line: string) =
 let instructions =
     readLines "Day2.txt" |> List.map parseLine
 
+let testInstructions =
+    [ "forward 5"
+      "down 5"
+      "forward 8"
+      "up 3"
+      "down 8"
+      "forward 2" ]
+    |> List.map parseLine
+
+let logPosition position =
+    printfn $"{position}"
+    position
+
 let move startPosition instruction =
     match instruction.Move with
     | Forward ->
         { startPosition with
-              Horizontal = startPosition.Horizontal + instruction.Distance }
+              Horizontal = startPosition.Horizontal + instruction.Distance
+              Depth =
+                  startPosition.Depth
+                  + startPosition.Aim * instruction.Distance }
     | Up ->
         { startPosition with
-              Depth = startPosition.Depth - instruction.Distance }
+              Aim = startPosition.Aim - instruction.Distance }
     | Down ->
         { startPosition with
-              Depth = startPosition.Depth + instruction.Distance }
+              Aim = startPosition.Aim + instruction.Distance }
 
 let rec processInstructions startPosition instructions =
     match instructions with
     | [] -> startPosition
-    | head :: tail -> processInstructions (move startPosition head) tail
+    | instruction :: tail ->
+        let newPosition = move startPosition instruction
+        printfn $"{startPosition} -> {instruction} -> {newPosition}"
+        processInstructions newPosition tail
 
 let run () : unit =
     let finalPosition =
-        processInstructions { Horizontal = 0; Depth = 0 } instructions
+        processInstructions { Horizontal = 0; Depth = 0; Aim = 0 } instructions
 
     printfn $"End position: {finalPosition}"
     printfn $"Result: {finalPosition.Horizontal * finalPosition.Depth}"
